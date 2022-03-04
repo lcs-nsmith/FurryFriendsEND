b2ae933cdd522374c2ec4634d0ff7eb677109140
Nathan Smith <nsmith@lcs.on.ca>
2022-03-04 17:16:46 -0500

    Initial Commit

---
+++ FurryFriends/Model/DogPathEndpoint.swift
@@ -0,0 +1,12 @@
+//
+//  DogPathEndpoint.swift
+//  FurryFriends
+//
+//  Created by Nathan Smith on 2022-03-04.
+//
+
+import Foundation
+
+struct DogPathEndpoint: Decodable, Hashable, Encodable {
+    let message: String
+}
